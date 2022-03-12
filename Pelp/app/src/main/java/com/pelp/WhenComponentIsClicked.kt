522be621ad00a47e7ff92deec0ec48821d547dad
Lyle <lylelopez03@gmail.com>
2022-03-11 19:30:57 -0800

    my files made

---
+++ Pelp/app/src/main/java/com/pelp/WhenComponentIsClicked.kt
@@ -0,0 +1,18 @@
+package com.pelp
+
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun button(){
+
+        Text(text = "Hello")
+
+}
+@Preview
+@Composable
+fun buttonPreview(){
+        button()
+}
